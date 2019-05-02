75ecc5318f71fef28dd7456b0c9334af3f6c53ce
John Talman <JohnDTalman@gmail.com>
2019-05-02 07:52:47 -0400

    "discriminant", and determinant.

--- examples/EMTEAPOT0/postProcess/betas.c
+++ examples/EMTEAPOT0/postProcess/betas.c
@@ -11,7 +11,8 @@ int main (int argc, char*argv[])
  double R11,R21,R12,R22;
  double Rtr=0;
  int numSP;// = processSP(1);
- double cosMu=0,det=0,a0=0,b0=0;
+ double cosMu=0,dsc=0,a0=0,b0=0;
+ double det=0;
  double sinMu=0;
  double Mu=0,Mu_PR=0,Q=0;
 
@@ -27,15 +28,28 @@ int main (int argc, char*argv[])
  printf("SP3 lines %d:  SP3x[numSP3-1]/x1typ (R12) %+e\n",numSP,R12);
  printf("SP3 lines %d: SP3xP[numSP3-1]/x1typ (R22) %+e\n\n",numSP,R22);
 
+/*
  Rtr=R11+R22;
  cosMu=Rtr/2.;
  det=1.-Rtr*Rtr/4.;
  if( (det<0) ){printf("X: Trying to take square root of a negative number!\n");exit(1);};
  b0=R12/sqrt(det); 
+ sinMu=R21/b0;
+ a0=(R11-R22)/2./sinMu;
+*/
+
+ Rtr=R11+R22;
+ cosMu=Rtr/2.;
+ dsc=1.-Rtr*Rtr/4.;
+ if( (dsc<0) ){printf("X: Trying to take square root of a negative number!\n");exit(1);}
+ else{ printf("'discriminant' %+.17g so mu can be inferred\n\n",dsc);}
+ b0=R12/sqrt(dsc); 
  sinMu=R12/b0;
  a0=(R11-R22)/2./sinMu;
 
- printf("trace %+e\n",Rtr);
+ det=R11*R22-R12*R21;
+
+ printf("\ntrace %+e\n",Rtr);
  printf("determinant %+.17g\n\n",det);
 
  printf("betaX %+e\n",b0);
